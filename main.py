dcf826f28927b823db6b95066d26979025c816be
alinoleumm <albina.li@nu.edu.kz>
2019-03-12 17:24:56 +0600

    added html page

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template            
+# https://medium.com/@sightengine_/image-upload-and-moderation-with-python-and-flask-e7585f43828a
+
+from flask import Flask, render_template, request            
 import subprocess 
 
 import sys
@@ -14,6 +16,13 @@ from torch.autograd import Variable
 import numpy as np
 from PIL import Image
 from sklearn.externals import joblib
+
+app = Flask(__name__)
+
+algorithms = ['RetinaNet', 'SSD', 'MaskR-CNN', 'MultiPath Network', 'R-FCN', 'YOLO']
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 clf = joblib.load('/mnt/images/MSCOCO/result_val2014/svc.sav') # PATH TO PRE-TRAINED MODEL
 model = models.alexnet(pretrained=True)
@@ -54,17 +63,26 @@ def choose(fname, context):
     algo = clf.predict(np.hstack((features,context)).reshape(1, -1))[0]
     return algo
 
-app = Flask(__name__)
-
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/algo")
-def algorithm():
-    # Provide a filename and context (front-end)
-	algo = choose('/home/alinoleumm/images/42.jpg', [0]*8)
-	return str(algo)
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # ADD RESTRICTIONS TO FILE EXTENSIONS
+    place = int(request.form['place'])
+    inside = int(request.form['inside'])
+    outside = int(request.form['outside'])
+    filled = int(request.form['filled'])
+    light = int(request.form['light'])
+    surrounding = int(request.form['surrounding'])
+    time = int(request.form['time'])
+    action = int(request.form['action'])
+    file = request.files['image'] 
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(f)
+    algo = choose(os.path.join(app.config['UPLOAD_FOLDER'], file.filename), [place,inside,outside,filled,light,surrounding,time,action])
+    return 'Best algorithm for this image is ' + algorithms[algo-1] + '.'
 
 if __name__ == "__main__":
     app.run(debug=True)
